3c516cd68a2fc29e23ced1c282c3c9021a00ad96
kris <kris.xiong@dhssys.com>
2024-06-26 16:23:18 +0800

    初始化amazon sdk新工程

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # amazon-sp-api
 amazon sp api 自定义 sdk
+
+官方model工程：https://github.com/amzn/selling-partner-api-models/
---
+++ sellerpartner-api-sdk/src/main/java/com/amazon/spapi/model/seller/Zhanwei.java
@@ -0,0 +1,9 @@
+package com.amazon.spapi.model.seller;
+
+/**
+ * @USER: Kris
+ * @DATE: 2024/6/26
+ * @Description
+ **/
+public class Zhanwei {
+}
